fc7f4b8fd553fe81a03ca1a7eff2eedb71ac9da3
Nikkieeex <nicolebovenga004@gmail.com>
2025-03-13 09:45:20 +0100

    Jouw beschrijving hier, bijv. 'Update Flask project structuur'

--- Website/static/py/converter.py
+++ Website/static/py/converter.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Lees het quant.sf bestand
-input_file = "../voorbeeld_data/quant.sf"
+input_file = "../../voorbeeld_data/quant.sf"
 df = pd.read_csv(input_file, sep="\t")
 
 # Selecteer interessante kolummen
@@ -9,7 +9,7 @@ selected_columns = ["Name", "TPM"]
 df_selected = df[selected_columns]
 
 # Opslaan als JSON
-json_output_file = "../voorbeeld_data/quant.json"
+json_output_file = "../../voorbeeld_data/quant.json"
 df_selected.to_json(json_output_file, orient="records", indent=4)
 
 print(f'Output geschreven naar: {json_output_file}')

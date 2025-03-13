26fa995aafc5b019dca91efff017dd9140735e08
Joris Deelstra <67652798+MrJoruz@users.noreply.github.com>
2025-03-13 16:09:35 +0100

    Spelfout verbeterd

--- Website/app.py
+++ Website/app.py
@@ -9,23 +9,23 @@ def home():  # put application's code here
 
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home', activate_page='index')
+    return render_template('index.html', title='Home', active_page='index')
 
 @app.route('/salmon_invoer')
 def salmon_invoer():
     if request.method == 'GET':
-        return render_template('salmon_invoer.html', title='Salmon Invoer', activate_page='salmon_invoer')
+        return render_template('salmon_invoer.html', title='Salmon Invoer', active_page='salmon_invoer')
     elif request.method == 'POST':
         kwargs = request.form
-        return render_template('resultaat.html', **kwargs, title='Resultaat')
+        return render_template('resultaat.html', **kwargs, title='Resultaat', active_page='resultaat')
 
 @app.route('/uitleg')
 def uitleg():
-    return render_template('uitleg.html', title='Uitleg', activate_page='uitleg')
+    return render_template('uitleg.html', title='Uitleg', active_page='uitleg')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', title='Contact', activate_page='contact')
+    return render_template('contact.html', title='Contact', active_page='contact')
 
 @app.errorhandler(404)
 def page_not_found(e):
